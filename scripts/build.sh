54a71a8618af5c028d8ccde04458e264da9d5bb4
Basil Crow <me@basilcrow.com>
2026-05-28 10:46:07 -0700

    Add CI build and Font Bakery lint job (#28)

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,16 @@ for name in Neanes NeanesRTL NeanesStathisSeries; do
 	fontforge -script ../scripts/generate-font-metadata.py ../sources/${name}.sfd ../metadata/glyphnames.json
 done
 
-cp *.metadata.json ../fonts
-cp *.metadata.json ../../neanes/src/assets/fonts
-rm *.metadata.json
+cp ./*.metadata.json ../fonts
+if [ -d ../../neanes/src/assets/fonts ]; then
+	cp ./*.metadata.json ../../neanes/src/assets/fonts
+fi
+rm ./*.metadata.json
 
 cp ../fonts/Neanes.otf ../docs/_media
-cp ../fonts/Neanes.otf ../../neanes/docs/.vitepress/theme/assets/fonts
-cp ../fonts/*.otf ../../neanes/src/assets/fonts
+if [ -d ../../neanes/docs/.vitepress/theme/assets/fonts ]; then
+	cp ../fonts/Neanes.otf ../../neanes/docs/.vitepress/theme/assets/fonts
+fi
+if [ -d ../../neanes/src/assets/fonts ]; then
+	cp ../fonts/*.otf ../../neanes/src/assets/fonts
+fi
