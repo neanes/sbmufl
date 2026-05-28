d32940c65e838b80ad3798d5f2f17a36fe73860d
Basil Crow <me@basilcrow.com>
2026-05-28 09:51:00 -0700

    Add Unix shebang to shell script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 for name in Neanes NeanesRTL NeanesStathisSeries; do
-    fontforge -script ../scripts/generate-font.py ../sources/${name}.sfd ../fonts/${name}.otf
-    fontforge -script ../scripts/generate-font-metadata.py ../sources/${name}.sfd ../metadata/glyphnames.json
+	fontforge -script ../scripts/generate-font.py ../sources/${name}.sfd ../fonts/${name}.otf
+	fontforge -script ../scripts/generate-font-metadata.py ../sources/${name}.sfd ../metadata/glyphnames.json
 done
 
 cp *.metadata.json ../fonts
@@ -10,4 +12,3 @@ rm *.metadata.json
 cp ../fonts/Neanes.otf ../docs/_media
 cp ../fonts/Neanes.otf ../../neanes/docs/.vitepress/theme/assets/fonts
 cp ../fonts/*.otf ../../neanes/src/assets/fonts
-
