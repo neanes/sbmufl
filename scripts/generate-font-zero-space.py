29b01e9d22a168009f7210b4a9ce9333a31296b3
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2026-06-11 16:22:55 -0500

    Remove yporroi.gorgon spacing

--- scripts/generate-font-zero-space.py
+++ scripts/generate-font-zero-space.py
@@ -4,15 +4,6 @@ import json
 import fontforge
 import psMat
 
-STANDARD_GLUE_WIDTH = 130  # TODO this should be read from the metadata
-GORGON_TOP_ANCHOR = "gorgonTop"
-
-YPORROI_GORGON_MARKS = {
-    0xF009: "gorgonAbove",
-    0xF00A: "digorgon",
-    0xF00B: "trigorgon",
-}
-
 BARLINE_THESEOS_CODEPOINTS = {
     0xE212,  # barlineTheseos
     0xE215,  # barlineShortTheseos
@@ -95,75 +86,26 @@ if __name__ == "__main__":
             and not (0xE2A0 <= char.unicode <= 0xE42F)
             and not (0xF003 <= char.unicode <= 0xF004)
         ):
-            mark_name = YPORROI_GORGON_MARKS.get(char.unicode)
-
-            if mark_name is not None:
-                if TEMP_GLYPH_NAME in font:
-                    font.removeGlyph(TEMP_GLYPH_NAME)
-
-                temp = font.createChar(-1, TEMP_GLYPH_NAME)
-                mark = font[mark_name]
-                base_anchor_x, base_anchor_y = get_anchor_position(
-                    char, GORGON_TOP_ANCHOR, {"base", "basechar"}
-                )
-                mark_anchor_x, mark_anchor_y = get_anchor_position(
-                    mark, GORGON_TOP_ANCHOR, {"mark"}
-                )
-                mark_transform = (
-                    1,
-                    0,
-                    0,
-                    1,
-                    base_anchor_x - mark_anchor_x,
-                    base_anchor_y - mark_anchor_y,
-                )
-
-                temp.addReference(char.glyphname)
-                temp.addReference(mark_name, mark_transform)
-
-                while temp.references:
-                    temp.unlinkRef()
-
-                combined_xmin, ymin, combined_xmax, ymax = temp.boundingBox()
-                combined_xmin = int(round(combined_xmin))
-                combined_xmax = int(round(combined_xmax))
-
-                while char.references:
-                    char.unlinkRef()
-
-                base_xmin, ymin, base_xmax, ymax = char.boundingBox()
-                base_xmin = int(round(base_xmin))
-                base_xmax = int(round(base_xmax))
-
-                char.left_side_bearing = base_xmin - combined_xmin
-                char.right_side_bearing = (
-                    combined_xmax - base_xmax - STANDARD_GLUE_WIDTH
-                )
-
-                font.removeGlyph(TEMP_GLYPH_NAME)
-                continue
-
             while char.references:
                 char.unlinkRef()
 
-            if mark_name is None:
-                if char.unicode in BARLINE_THESEOS_CODEPOINTS:
-                    barline_bbox = get_lowest_contour_bbox(char)
+            if char.unicode in BARLINE_THESEOS_CODEPOINTS:
+                barline_bbox = get_lowest_contour_bbox(char)
 
-                    if barline_bbox is None:
-                        font.selection.select(char)
-                        font.autoWidth(0)
-                    else:
-                        xmin, ymin, xmax, ymax = barline_bbox
-                        barline_width = round(xmax - xmin)
-
-                        # Move the glyph so the barline contour starts at x=0.
-                        # The yfen may remain outside the advance width.
-                        char.transform(psMat.translate(-xmin, 0))
-                        char.width = barline_width
-                else:
+                if barline_bbox is None:
                     font.selection.select(char)
                     font.autoWidth(0)
+                else:
+                    xmin, ymin, xmax, ymax = barline_bbox
+                    barline_width = round(xmax - xmin)
+
+                    # Move the glyph so the barline contour starts at x=0.
+                    # The yfen may remain outside the advance width.
+                    char.transform(psMat.translate(-xmin, 0))
+                    char.width = barline_width
+            else:
+                font.selection.select(char)
+                font.autoWidth(0)
 
     font.generate(args.outfile_otf)
 
